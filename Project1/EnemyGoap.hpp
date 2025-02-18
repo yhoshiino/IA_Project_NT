477ff103aab88e90a59c9f1b5c5c409366bd1025
Thomlight <tmillet@gaming.tech>
2025-02-18 14:55:01 +0100

    added goap enemy

---
+++ Project1/EnemyGoap.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef ENEMYGOAP_HPP
+#define ENEMYGOAP_HPP
+
+#include <memory>
+#include <vector>
+#include "State.hpp"
+#include "Action.hpp"
+#include "PatrolAction.hpp"
+#include "FleeAction.hpp"
+#include "FollowAction.hpp"
+#include "AttackAction.hpp"
+#include "Entity.hpp"
+
+class EnemyGoap : public Entity {
+private:
+    State currentState;
+    std::vector<std::shared_ptr<Action>> actions;
+
+public:
+    EnemyGoap(sf::Vector2f position, bool sight, bool range, bool health);
+    void InitializeActions();
+    void UpdateState(bool sight, bool range, bool health);
+    void ExecuteAction();
+    void update(float deltaTime, Grid& grid, sf::Vector2f playerPosition) override;
+};
+
+#endif
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -8,6 +8,7 @@
 #include "AttackAction.hpp"
 #include "FleeAction.hpp"
 #include "Planner.hpp"
+#include "EnemyGoap.hpp"
 #include <vector>
 #include <memory>
 
@@ -21,35 +22,11 @@ int main() {
     RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Jeu SFML - IA Ennemis");
     window.setFramerateLimit(60);
 
-    // Initialize actions
-    vector<shared_ptr<Action>> actions = {
-        make_shared<PatrolAction>(),
-        make_shared<FollowAction>(),
-        make_shared<AttackAction>(),
-        make_shared<FleeAction>()
-    };
-
-    // Test different states
-    vector<State> testStates = {
-        State(false, false, false), // Patrolling
-        State(true, false, false),  // Following
-        State(true, true, false),   // Attacking
-        State(true, true, true)     // Fleeing
-    };
-
-    for (State& state : testStates) {
-        shared_ptr<Action> action = Plan(state, actions);
-        if (action) {
-            action->Execute(state);
-        }
-        else {
-            cout << "No action can be executed.\n";
-        }
-    }
-
-
     Player player(Vector2f(200, 400));
-    vector<Enemy> enemies = { Enemy(Vector2f(100, 100)), Enemy(Vector2f(700, 100)) };
+    vector<shared_ptr<EnemyGoap>> enemies = {
+        make_shared<EnemyGoap>(Vector2f(100, 100), false, false, false)
+    };
+
     Grid grid;
     grid.loadFromFile("map.txt");
 
@@ -66,19 +43,19 @@ int main() {
         }
 
         player.update(deltaTime, grid, player.position);
+
         for (auto& enemy : enemies) {
-            enemy.update(deltaTime, grid, player.position);
+            enemy->update(deltaTime, grid, player.position);
         }
 
         window.clear();
         grid.draw(window);
         window.draw(player.shape);
         for (const auto& enemy : enemies) {
-            window.draw(enemy.shape);
+            window.draw(enemy->shape);
         }
-            
+
         window.display();
     }
     return 0;
 }
-
